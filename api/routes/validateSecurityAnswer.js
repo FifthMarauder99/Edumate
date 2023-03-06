2bafc43cd40949ac7bfbb409052d34869c4f478c
zhanso <zhanso@iu.edu>
2023-03-05 21:29:29 -0500

    added function to parse security question and answer for apostrophes registration fixed

--- api/routes/validateSecurityAnswer.js
+++ api/routes/validateSecurityAnswer.js
@@ -3,6 +3,19 @@ var router = express.Router();
 const { Pool, Query } = require('pg');
 
 // validating security answer with given security question
+
+function double_apostrophe(str) {
+    doubled = ''
+    for (const c of str) {
+        if (c === '\'') {
+            doubled += c + c;
+        } else {
+            doubled += c;
+        }
+    }
+    return doubled;
+}
+
 router.post('/', async (req, res) => {
     try {
         const pool = new Pool({
@@ -10,9 +23,11 @@ router.post('/', async (req, res) => {
             ssl: (process.env.REACT_APP_DATABASE_URL) ? {rejectUnauthorized : false} : true,
         });
         const client = await pool.connect();
+        const proper_question = double_apostrophe(req.body.securityQuestion);
+        const proper_answer = double_apostrophe(req.body.securityAnswer);
         const query = `SELECT EXISTS(
-                        SELECT * FROM users WHERE security_question = '${req.body.securityQuestion}'
-                        AND security_answer = '${req.body.securityAnswer}'
+                        SELECT * FROM users WHERE security_question = '${proper_question}'
+                        AND security_answer = '${proper_answer}'
                         )`; 
         const result = await client.query(query);
         const results = { 'results': (result) ? result.rows : null}; // { results: [ { exists: true } ] }
