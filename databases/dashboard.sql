3d3eff842a47997e15c142395286d92679ebebe5
Adesh <aoak@iu.edi>
2023-04-06 23:06:35 -0400

    updated dashboard databases after restructuring the schema

--- databases/dashboard.sql
+++ databases/dashboard.sql
@@ -1,75 +1,89 @@
-
-/*CREATE TABLE Students (
-    ID SERIAL PRIMARY KEY,
-    student_id INTEGER UNIQUE NOT NULL,
-    student_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (student_id) REFERENCES users (user_id)
+/*This table stores the information that will not change every year about a course*/
+CREATE TABLE courses (
+    course_id VARCHAR(20) PRIMARY KEY,
+    course_title VARCHAR(255),
+    course_description TEXT,
 );
 
-CREATE TABLE Professors (
-    ID SERIAL PRIMARY KEY,
-    prof_id INTEGER UNIQUE NOT NULL,
-    professor_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (prof_id) REFERENCES users (user_id)
+/* This table is created as one course can be taught multiple times over years*/
+CREATE TABLE courses_per_semester (
+    course_id VARCHAR(20),
+    semester_id VARCHAR(20),
+    course_start_date DATE,
+    course_end_date DATE,
+    PRIMARY KEY(course_id,cycle_id)
+)
+
+/*This table keeps track of all semesters like fall 2022,spring 2023 etc....*/
+CREATE TABLE semesters(
+    semester_id VARCHAR(20) PRIMARY KEY,
+    semester VARCHAR(50),
+    semester_start_date DATE,
+    semester_end_date DATE
+)
+
+/* So when we assign a student to a course through admin panel this table 
+should be updated */
+CREATE TABLE enrollments (
+    course_id VARCHAR(20) NOT NULL,
+    semester_id VARCHAR(20) NOT NULL,
+    user_id INTEGER NOT NULL,
+    enrollment_date DATE,
+    PRIMARY KEY (course_id,semester_id,student_id)
 );
 
-CREATE TABLE Admins (
-    ID SERIAL PRIMARY KEY,
-    admin_id INTEGER UNIQUE NOT NULL,
-    admin_name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (admin_id) REFERENCES users (user_id)
-);*/
-
-CREATE TABLE Courses (
-    ID SERIAL PRIMARY KEY,
-    Title VARCHAR(255) NOT NULL,
-    Description TEXT,
-    Professor_id INTEGER NOT NULL,
-    Start_Date DATE NOT NULL,
-    End_Date DATE NOT NULL,
-    capacity INTEGER NOT NULL,
-    FOREIGN KEY (Professor_id) REFERENCES Professors (prof_id)
+/* So when we assign a professor to a course through admin panel this table 
+should be updated */
+CREATE TABLE courseAssignment_professor (
+    course_id VARCHAR(20) NOT NULL,
+    semester_id VARCHAR(20) NOT NULL,
+    user_id INTEGER NOT NULL,
+    PRIMARY KEY (course_id,semester_id,student_id)
 );
 
-CREATE TABLE Enrollments (
-    ID SERIAL PRIMARY KEY,
-    User_id INTEGER NOT NULL,
-    Course_id INTEGER NOT NULL,
-    Enrolled_At TIMESTAMP NOT NULL,
-    Completion_Date TIMESTAMP,
-    FOREIGN KEY (User_id) REFERENCES Students (student_id),
-    FOREIGN KEY (Course_id) REFERENCES Courses (ID)
-);
+INSERT INTO courses(course_id,title,course_description)
+VALUES ('CSCI-P565','Software Engineering 1','Learn Software Engineering, Agile Methodologies and develop a full stack application!');
 
-CREATE TABLE Materials (
-    ID SERIAL PRIMARY KEY,
-    Title VARCHAR(255) NOT NULL,
-    Description TEXT,
-    Type VARCHAR(255) NOT NULL,
-    Course_id INTEGER NOT NULL,
-    Created_At TIMESTAMP NOT NULL,
-    Content TEXT,
-    FOREIGN KEY (Course_id) REFERENCES Courses (ID)
-);
+INSERT INTO courses(course_id,title,course_description)
+VALUES ('CSCI-B505','Applied Algorithms','Learn everything about algorithms!');
 
-CREATE TABLE Assignments (
-    ID SERIAL PRIMARY KEY,
-    Title VARCHAR(255) NOT NULL,
-    Description TEXT,
-    Deadline TIMESTAMP NOT NULL,
-    Course_ID INTEGER NOT NULL,
-    FOREIGN KEY (Course_ID) REFERENCES Courses (ID)
-);
+INSERT INTO courses(course_id,title,course_description)
+VALUES ('CSCI-P556','Applied Machine Learning','Let us teach machines how to do our task so we can chill!');
 
-CREATE TABLE Assignment_Grades (
-    ID SERIAL PRIMARY KEY,
-    Enrollment_id INTEGER NOT NULL,
-    Assignment_id INTEGER NOT NULL,
-    Grade INTEGER NOT NULL,
-    FOREIGN KEY (Enrollment_id) REFERENCES Enrollments (ID),
-    FOREIGN KEY (Assignment_id) REFERENCES Assignments (ID)
-);
+INSERT INTO courses(course_id,title,course_description)
+VALUES ('CSCI-B565','Data Mining','Learn to infer data and observe interesting patterns!');
+
+INSERT INTO courses(course_id,title,course_description)
+VALUES ('DSCI-D532','Applied Database technologies','Learn everything about Databases!');
 
 
+INSERT INTO semesters(semester_id,semester,semester_start_date,semester_end_date)
+VALUES ('FA22','Fall 2022','2022-08-22','2022-12-16');
+INSERT INTO semesters(semester_id,semester,semester_start_date,semester_end_date)
+VALUES ('SP23','Spring 2023','2023-01-09','2023-05-05');
+
+
+INSERT INTO courses_per_semester(course_id,semester_id,course_start_date,course_end_date)
+VALUES ('CSCI-B505','FA22','2022-08-22','2022-12-16');
+INSERT INTO courses_per_semester(course_id,semester_id,course_start_date,course_end_date)
+VALUES ('CSCI-P556','FA22','2022-08-22','2022-12-16');
+INSERT INTO courses_per_semester(course_id,semester_id,course_start_date,course_end_date)
+VALUES ('CSCI-P565','SP23','2023-01-09','2023-05-05');
+INSERT INTO courses_per_semester(course_id,semester_id,course_start_date,course_end_date)
+VALUES ('DSCI-D532','SP23','2023-01-09','2023-05-05');
+
+
+INSERT INTO TABLE enrollments(course_id,semester_id,user_id,enrollment_date)
+VALUES('CSCI-P565','SP23',1,'2022-12-10');
+INSERT INTO TABLE enrollments(course_id,semester_id,user_id,enrollment_date)
+VALUES('CSCI-P565','SP23',4,'2022-12-10');
+INSERT INTO TABLE enrollments(course_id,semester_id,user_id,enrollment_date)
+VALUES('CSCI-P556','FA22',1,'2022-05-05')
+
+
+INSERT INTO TABLE courseAssignment_professor(course_id,semester_id,user_id)
+VALUES('CSCI-P565','SP23',5)
+INSERT INTO TABLE courseAssignment_professor(course_id,semester_id,user_id)
+VALUES('CSCI-P556','FA22',6)
 
 
